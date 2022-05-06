4c99f7e49fad4a7e0d9885779b42f36e5aca8c81
Javed Alam <alam9120573582@gmail.com>
2022-05-06 15:53:57 +0530

    Silent bid program to provide the winner of the auction

---
+++ silentBid.py
@@ -0,0 +1,13 @@
+name = input("Enter bidder's Name: ")
+bid = int(input("Enter amount: "))
+auction = {}
+choice = input("Are there other bidders (yes/no): ")
+auction[bid] = name
+while (choice == "yes"):
+    name = input("Enter bidder's Name: ")
+    bid = int(input("Enter amount: "))
+    choice = input("Are there other bidders (yes/no): ")
+    auction[bid] = name
+
+val = sorted(auction.keys())
+print(auction.get(val[len(val)-1]))
