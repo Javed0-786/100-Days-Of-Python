0813853a9b2dcf290202f5d8d749ada25db397b1
Javed Alam <alam9120573582@gmail.com>
2022-06-13 17:21:45 +0530

    Day 35 Completed

--- Day34/302 quizzler-app-start/ui.py
+++ Day34/302 quizzler-app-start/ui.py
@@ -11,25 +11,50 @@ class QuizInterface:
         self.window.title("Quizzler")
         self.window.config(padx=20, pady=20, bg=THEME_COLOR)
         self.quiz = quiz
-        self.score = Label(text="Score: 0", font=(
+        self.score = Label(text=f"Score: {self.quiz.score}", font=(
             "arial", 15, "normal"), bg=THEME_COLOR, fg="white")
         self.score.grid(row=0, column=1)
 
         self.canvas = Canvas(width=300, height=250, bg="white")
         self.question = self.canvas.create_text(
-            150, 125, text="Some Question Text", font=("arial", 20, "italic"), fill=THEME_COLOR)
+            150, 125, text="Some Question Text", font=("arial", 20, "italic"), fill=THEME_COLOR, width=300)
         self.canvas.grid(row=1, column=0, columnspan=2, padx=20, pady=20)
 
         true_image = PhotoImage(file="images/true.png")
         false_image = PhotoImage(file="images/false.png")
 
-        self.true = Button(image=true_image, highlightthickness=0)
+        self.true = Button(
+            image=true_image, highlightthickness=0, command=self.return_true)
         self.true.grid(row=2, column=0, padx=20, pady=20, )
-        self.false = Button(image=false_image, highlightthickness=0)
+        self.false = Button(image=false_image,
+                            highlightthickness=0, command=self.return_false)
         self.false.grid(row=2, column=1, padx=20, pady=20, )
         self.get_next_question()
         self.window.mainloop()
 
     def get_next_question(self):
-        q_text = self.quiz.next_question()
-        self.canvas.itemconfig(self.question, text=q_text)
+        self.canvas.config(bg="white")
+        if self.quiz.still_has_questions():
+            q_text = self.quiz.next_question()
+            self.canvas.itemconfig(self.question, text=q_text)
+            self.score.config(text=f"Score: {self.quiz.score}")
+        else:
+            self.canvas.itemconfig(
+                self.question, text="You have attempted all the questions.\nThankyou")
+            self.true.config(state="disabled")
+            self.false.config(state="disabled")
+
+    def return_true(self):
+        is_right = self.quiz.check_answer("True")
+        self.delay(is_right)
+
+    def return_false(self):
+        is_wrong = self.quiz.check_answer("False")
+        self.delay(is_wrong)
+
+    def delay(self, answer):
+        if answer:
+            self.canvas.config(bg="green")
+        else:
+            self.canvas.config(bg="red")
+        self.window.after(1000, self.get_next_question)
---
+++ Day35/Rain Alert/main.py
@@ -0,0 +1,44 @@
+import requests
+from twilio.rest import Client
+
+records = []
+
+OWN_EndPoint = "https://api.openweathermap.org/data/2.5/onecall?"
+api_key = "d35f98f7bd41b8edbe31e39fe5afa888"
+
+# AC7c1e84976efcaf0b1f60f5b323ca4692
+account_sid = "AC7c1e84976efcaf0b1f60f5b323ca4692"
+# 81a56a30224f6c048e9c4f3365e16aac
+account_token = "81a56a30224f6c048e9c4f3365e16aac"
+api_key_twilio = "SK8b35f4deb9d3b945ee3a8f2a553c2a7d"
+# sid = SKfab2f393533fb1e7b361401de6219362
+
+# GYKgtDxjdePCK5RZWkM9TXqkKoxEkQ2X || f6BMjgZp0UaJZQchBaarxYutZM36vo0N
+
+
+weather_params = {
+    "lat": 25.7,
+    "lon": 92.45,
+    "exclude": "current,minutely,daily",
+    "appid": api_key
+}
+
+response = requests.get(OWN_EndPoint, params=weather_params)
+data = response.json()["hourly"]
+
+
+records = [record for record in data if data.index(record) < 12]
+ids = [id['weather'][0]["id"] for id in records]
+for id in ids:
+    if id <= 700:
+        check = True
+
+if check == True:
+    client = Client(account_sid, account_token, api_key_twilio)
+    message = client.messages.create(
+        body="It's going to rain 🌦️ today. Remember to bring an 🌂☂️☔", from_="+19895141558", to="+917076759685")
+    print(message.status)
+
+# from='+19895141558'
+# body = "It's going to rain 🌦️ today. Remember to bring an ☔🌂☂️"
+# to = "+918887876270"
